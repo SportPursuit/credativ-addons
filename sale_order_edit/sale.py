070e868f66415f62482998c49b007d8d8844e66b
Michele Sarigu <michele.sarigu@sportpursuit.co.uk>
2019-09-12 11:46:42 +0100

    removing cancel line in the edited order

--- sale_order_edit/sale.py
+++ sale_order_edit/sale.py
@@ -291,14 +291,13 @@ class SaleOrderLine(osv.osv):
     }
 
     def copy_data(self, cr, uid, id_, default=None, context=None):
-        default = default or {}
-        line = self.pool.get('sale.order.line').browse(cr, uid, id_, context)
-        # preserving state for canceled sale order line
-        if line.state == 'cancel':
-            default.update({'state': 'cancel'})
+        original_line = self.pool.get('sale.order.line').browse(cr, uid, id_, context)
+        # removing cancel line in the edited order
+        if original_line.state == 'cancel':
+             return {}
 
         res = super(SaleOrderLine, self).copy_data(cr, uid, id_, default, context=context)
-        res.update({'order_line_edit_id' : id_})
+        res.update({'order_line_edit_id': id_})
         return res
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
